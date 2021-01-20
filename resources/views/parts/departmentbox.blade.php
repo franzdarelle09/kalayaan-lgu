884840a7f85671d6286b123852bc05eb90bab121
Franz Darelle Macatangga <franzmacatangga2@gmail.com>
2021-01-20 11:03:44 +0800

    change services

--- resources/views/parts/departmentbox.blade.php
+++ resources/views/parts/departmentbox.blade.php
@@ -1,13 +1,16 @@
  <!--Department Box Start-->
  <div class="col-md-4 col-sm-4">
     <div class="department-box mb30 c1">
-      <h6>Agriculture & Food</h6>
+      <h6>SOCIAL SECTOR</h6>
       <ul>
-        <li><a href="#"><i class="fas fa-circle"></i> Farming Sector</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Agri Industry Development</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Forestry</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Rural Environment</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Food Safety</a></li>
+        <li><a href="#"><i class="fas fa-circle"></i> Health Services</a></li>
+        <li><a href="#"><i class="fas fa-circle vishidden"></i> (MHO, MNAO, Sanitation)</a></li>
+        <li><a href="#"><i class="fas fa-circle"></i> Support to Educational Services</a></li>
+        <li><a href="#"><i class="fas fa-circle vishidden"></i>(DepEd)</a></li>
+        <li><a href="#"><i class="fas fa-circle"></i> Peace, Security and Disaster</a></li>
+        <li><a href="#"><i class="fas fa-circle vishidden"></i>Risk Management</a></li>
+        <li><a href="#"><i class="fas fa-circle vishidden"></i>(PNP, MDRRMO, MSWDO)</a></li>
+        
       </ul>
       <a class="see-more" href="#">See More</a> </div>
   </div>
@@ -15,13 +18,15 @@
   <!--Department Box Start-->
   <div class="col-md-4 col-sm-4">
     <div class="department-box mb30 c2">
-      <h6>Employment Affairs</h6>
+      <h6>ECONOMIC SECTOR</h6>
       <ul>
-        <li><a href="#"><i class="fas fa-circle"></i> Find Your Job</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Employers & Employment</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Public Service Identity</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Retirements & Older People</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Work Licence Permit</a></li>
+        <li><a href="#"><i class="fas fa-circle"></i> Support to Agriculture Sector</a></li>
+        <li><a href="#"><i class="fas fa-circle vishidden"></i> (Agriculture)</a></li>
+        <li><a href="#"><i class="fas fa-circle"></i> Revenue Generation (MTO)</a></li>
+        <li><a href="#"><i class="fas fa-circle"></i> Enterprise, Business and </a></li>
+        <li><a href="#"><i class="fas fa-circle vishidden"></i>Industry Promotion</a></li>
+        <li><a href="#"><i class="fas fa-circle vishidden"></i>(MEE, Tourism)</a></li>
+        
       </ul>
       <a class="see-more" href="#">See More</a> </div>
   </div>
@@ -29,13 +34,12 @@
   <!--Department Box Start-->
   <div class="col-md-4 col-sm-4">
     <div class="department-box mb30 c3">
-      <h6>Housing & Land Use</h6>
+      <h6 style="font-size: 14px;">ENVIRONMENTAL SECTOR</h6>
       <ul>
-        <li><a href="#"><i class="fas fa-circle"></i> Residents & Commercial Areas</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Building Standards</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> National Planning Framework</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Water Filteration Authority</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> State Parks & Developments</a></li>
+        <li><a href="#"><i class="fas fa-circle"></i> Environmental Management</a></li>
+        <li><a href="#"><i class="fas fa-circle vishidden"></i>(MENRO)</a></li>
+        
+        
       </ul>
       <a class="see-more" href="#">See More</a> </div>
   </div>
@@ -43,13 +47,12 @@
   <!--Department Box Start-->
   <div class="col-md-4 col-sm-4">
     <div class="department-box c4">
-      <h6>Health & Social Services</h6>
+      <h6 style="font-size: 12px;">INFRASTRUCTURE GOVERNANCE</h6>
       <ul>
-        <li><a href="#"><i class="fas fa-circle"></i> Hospitals & Emergencies</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Healty State Life</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Public Safety</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Police Department</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Fire Brigade & Rescue Officials</a></li>
+        <li><a href="#"><i class="fas fa-circle"></i>Development Planning </a></li>
+        <li><a href="#"><i class="fas fa-circle vishidden"></i>(Engineering, MPDO, </a></li>
+        <li><a href="#"><i class="fas fa-circle vishidden"></i>ASSESSOR(Housing)</a></li>
+        
       </ul>
       <a class="see-more" href="#">See More</a> </div>
   </div>
@@ -57,28 +60,18 @@
   <!--Department Box Start-->
   <div class="col-md-4 col-sm-4">
     <div class="department-box c5">
-      <h6 style="font-size: 12px;">Peace and Order and Public Safety</h6>
+      <h6 style="font-size: 12px;">INSTITUTIONAL GOVERNANCE</h6>
       <ul>
-        <li><a href="#"><i class="fas fa-circle"></i> Constitution & Bylaws</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Legal Services</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Courts Policy</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Children & Family Cases</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Equality & Integration</a></li>
+        <li><a href="#"><i class="fas fa-circle"></i> Local Legislation (SB)</a></li>
+        <li><a href="#" style="font-size:13px;"><i class="fas fa-circle"></i> Transparency (All Department, DILG)</a></li>
+        
+        <li><a href="#"><i class="fas fa-circle"></i> Participation (All Department)</a></li>
+        <li><a href="#"><i class="fas fa-circle"></i> Resource Allocation and </a></li>
+        <li><a href="#"><i class="fas fa-circle vishidden"></i> Utilization (MBO, MAccO, MTO)</a></li>
+        <li><a href="#"><i class="fas fa-circle"></i> Human Resource Management </a></li>
+        <li><a href="#"><i class="fas fa-circle vishidden"></i> and Development (HRMO)</a></li>
       </ul>
       <a class="see-more" href="#">See More</a> </div>
   </div>
   <!--Department Box End--> 
-  <!--Department Box Start-->
-  <div class="col-md-4 col-sm-4">
-    <div class="department-box c6">
-      <h6>Engineering Services</h6>
-      <ul>
-        <li><a href="#"><i class="fas fa-circle"></i> Civil Aviation</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Maritime Services</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Sustainable Transport</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Roads & Directions</a></li>
-        <li><a href="#"><i class="fas fa-circle"></i> Corporate Information</a></li>
-      </ul>
-      <a class="see-more" href="#">See More</a> </div>
-  </div>
-  <!--Department Box End--> 
+  
